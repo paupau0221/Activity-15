c62964f4a6781efd61579cc35875ff73afe47838
paupau0221 <paulinesoler2929@gmail.com>
2024-10-03 18:13:29 +0800

    fruit

--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -6,14 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './course.component.css'
 })
 export class CourseComponent {
-  courseList: string[] = ["BSIT", "COMSCI", "ACOUNTANCY"];
+  courseLit: string[] = ["COMSCI", "ACCOUNTANT", "BSIT"];
   item: string = '';
 
   addItems() {
-    this.courseList.push(this.item);
+    this.courseLit.push(this.item);
   }
 
   deleteCourse(index: number) {
-    this.courseList.splice(index, 1);
+    this.courseLit.splice(index, 1);
   }
 }
